884c1c5b01b1ba664e521c858bdb330317d78d4e
Tomer Ben-Arye <tomer.ben.arye@gmail.com>
2020-08-02 22:35:35 +0300

    try #3

--- slacki.py
+++ slacki.py
@@ -166,11 +166,10 @@ def register_callback():
     print("redirecting to timewatch")
     login_to_timewatch(response, action)
     # Notify user that we are handling the command, also without blocking
-    # text = """Your task was received and is being processed...\n
-    # *MANDATORY* - login to <checkin.timewatch.co.il/punch/punch2.php|timewatch> and check me."""
-    text = "Your task was received and is being processed..."
+    text = """Your task was received and is being processed...\n
+    *MANDATORY* - login to <checkin.timewatch.co.il/punch/punch2.php|timewatch> and check me."""
     print("sending msg to user")
-    send_message(cli, channel=action['user']['id'], blocks=text)
+    send_message(cli, text, action['user']['id'])
 
     return ACK
 
