02928db07dc4c0023392c3b20fc5d53c34d1e410
tobenary <tomer.ben.arye@gmail.com>
2020-08-19 11:09:37 +0300

    Update slacki.py

--- slacki.py
+++ slacki.py
@@ -76,7 +76,7 @@ def fill_time():
             "type": "plain_text_input",
             "placeholder": {
                 "type": "plain_text",
-                "text": "Enter your employee number"
+                "text": "Enter your employee number (usually 3 digits)"
             },
             "action_id": "username_value"
         },
@@ -120,7 +120,7 @@ def fill_time():
             "type": "section",
             "text": {
                 "type": "plain_text",
-                "text": "1. Before submitting the form, please fill your missing dates (if any)\n"
+                "text": "1. Before submitting the form, please fill your sick or vacation dates (if any)\n"
                         "2. After execution, I'm obligated to check the output in the timewatch site.\n"
                         "3. I am responsible to verify that all the data is accurate AFTER I click Fill Missing Times.",
                 "emoji": True
